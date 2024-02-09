89bd75b2fd06e60fb89af41b50372c9dbe773b2a
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2024-02-09 22:18:02 +0530

    map,filter,reduce

---
+++ 20_mapFilterReduce.js
@@ -0,0 +1,49 @@
+//map. filter, reduce
+
+let arr=[45,23,21];
+let a=arr.map((value)=>{
+    console.log(value);
+    return value+10;
+})
+console.log(a);
+console.log("-------------------------");
+a=arr.map((value,index,array)=>{
+    console.log(value,index,array);
+    return value+index;
+})
+console.log(a);
+
+//why use map??
+//--> to create a new array with some different values
+
+//difference betwee map and forEach
+/*
+---> map create a new array
+---> forEach doesnot creates a new array
+*/
+
+console.log("---------------------------------");
+
+//Array filter method
+let arr2=[45,23,21,0,3,5];
+let a2=arr2.filter((i)=>{
+    return i<10;
+})
+console.log(a2);
+
+//filter doesnot modify the original array
+
+console.log("---------------------------------");
+
+let arr3=[1,2,3,5,2,1];
+const red_func=(h1,h2)=>{
+    return h1+h2;
+}
+let newArr3=arr3.reduce(red_func);
+console.log(newArr3);
+//recomend explanation code with harry 
+//javascript lecture 20, Time-->10:30
+
+
+
+
