49b7335f0d054dd650c0f6455c9f1d1a4f522e8a
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2024-02-11 16:18:40 +0530

    DOM/BOM

---
+++ 28_DOM_BOM_Window.js
@@ -0,0 +1,9 @@
+//window is a global object
+
+console.log(window);
+
+//DOM---Document Object Model
+//BOM---Browser Object Model
+
+console.log(document);
+document.body.style.background="Yellow";
